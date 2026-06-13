006e90a9ffe66789234ea738c655883da983e910
yungestlavi <165472523+yungestlavi@users.noreply.github.com>
2026-06-13 13:05:00 +0200

    Double-click opens full USN change history (JournalTrace-style detail window)

--- SSForensic/Views/MainWindow.xaml.cs
+++ SSForensic/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Controls;
+using SSForensic.Models;
 
 namespace SSForensic.Views
 {
@@ -7,6 +11,26 @@ namespace SSForensic.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RecordsGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // Only react to double-clicks on an actual row, not headers/scrollbars.
+            if (e.OriginalSource is DependencyObject src && FindParentRow(src) == null)
+                return;
+
+            if (sender is DataGrid grid && grid.SelectedItem is ReplaceRecord rec)
+            {
+                var win = new UsnHistoryWindow(rec) { Owner = this };
+                win.Show();
+            }
+        }
+
+        private static DataGridRow FindParentRow(DependencyObject d)
+        {
+            while (d != null && d is not DataGridRow)
+                d = VisualTreeHelper.GetParent(d);
+            return d as DataGridRow;
         }
     }
 }
